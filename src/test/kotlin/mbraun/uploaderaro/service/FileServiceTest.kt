798fd9940e39fc5275e7ea364a434d1c43030517
BrownieBrown <61498988+BrownieBrown@users.noreply.github.com>
2021-04-13 14:13:24 +0200

    added test classes but without tests

---
+++ src/test/kotlin/mbraun/uploaderaro/service/FileServiceTest.kt
@@ -0,0 +1,38 @@
+package mbraun.uploaderaro.service
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class FileServiceTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @AfterEach
+    fun tearDown() {
+    }
+
+    @Test
+    fun store() {
+    }
+
+    @Test
+    fun getFile() {
+    }
+
+    @Test
+    fun getAllFiles() {
+    }
+
+    @Test
+    fun deleteById() {
+    }
+
+    @Test
+    fun getFileRepository() {
+    }
+}
